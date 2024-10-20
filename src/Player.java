f8ea754fdc402a1704e3f5ef93a88c07dd886a95
shenhaoGong <shenhaogong@cmail.carleton.ca>
2024-10-20 12:28:00 -0400

    Add Player.java

---
+++ src/Player.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class Player {
+    private String name;
+    private List<Tile> tiles; // playing card
+    private int remainingTurns; // turns left
+    private int currentScore; // current score
+    private int wins; // round of wins
+    private boolean isCurrentPlayer;
+
+
+    public Player(String name, int remainingTurns) {
+        this.name = name;
+        this.tiles = new ArrayList<Tile>(); //
+        this.remainingTurns = remainingTurns;
+        this.currentScore = 0;
+        this.wins = 0;
+        this.isCurrentPlayer = false; // not play in current round
+    }
+
+    // get player name
+    public String getName() {
+        return name;
+    }
+
+
+    public List<Tile> getTiles() {
+        return tiles;
+    }
+
+
+    public void addTile(Tile tile) {
+        tiles.add(tile);
+    }
+
+
+    public void removeTile(Tile tile) {
+        tiles.remove(tile);
+    }
+
+    // get how many round left
+    public int getRemainingTurns() {
+        return remainingTurns;
+    }
+
+    // reduce round
+    public void decrementTurns() {
+        if (remainingTurns > 0) {
+            remainingTurns--;
+        }
+    }
+
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    //add scores
+    public void updateScore(int points) {
+        currentScore += points;
+    }
+
+    // get rounds of wins
+    public int getWins() {
+        return wins;
+    }
+
+    // increase win round
+    public void incrementWins() {
+        wins++;
+    }
+
+
+    public boolean isCurrentPlayer() {
+        return isCurrentPlayer;
+    }
+
+
+    public void setCurrentPlayer(boolean isCurrentPlayer) {
+        this.isCurrentPlayer = isCurrentPlayer;
+    }
+}
